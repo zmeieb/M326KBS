4a88e8cd2c44a430b8208b5513cd85f29f93b204
Umiker <dino.neptun@gmail.com>
2019-04-05 14:17:46 +0200

    created classes (classdiagram)

---
+++ model/Room.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Room {
+
+    private Integer id;
+    private Integer numberOfFreePlaces;
+    private List<Row> rows = new ArrayList<>();
+
+    public String toString() {
+        return null;
+    }
+}
